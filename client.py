bb0fe91e58b24666aa9ff829aecb8a23bbf23872
Евгений Головень <eugene.goloven@gmail.com>
2020-12-02 09:00:28 +0200

    first v

--- client.py
+++ client.py
@@ -1,16 +1,326 @@
-# Import socket module 
-import socket                
+import socket
+import threading
+import time
+import sys
+
+ENRU = {
+	"I": "Я",
+"We": "Мы",
+"You": "Ты, вы",
+"He": "Он",
+"She": "Она",
+"It": "(о неодуш. предметах)",
+"They": "Они",
+"Come": "Приходить",
+"Do": "Делать",
+"Drink": "Пить",
+"Eat": "Есть",
+"Give": "Давать",
+"Go": "Идти",
+"Have": "Иметь",
+"Hear": "Слышать",
+"Help": "Помогать",
+"Like": "Нравиться",
+"Live": "Жить",
+"Put": "Класть, положить",
+"Say": "Сказать",
+"See": "Видеть",
+"Sit": "Сидеть",
+"Sleep": "Спать",
+"Stand": "Стоять",
+"Take": "Брать",
+"Use": "Пользоваться, использовать",
+"Want": "Хотеть",
+"Work": "Работать",
+"Boy": "Мальчик",
+"Friend": "Друг",
+"Girl": "Девочка",
+"Man": "Мужчина, человек",
+"People": "Люди",
+"Woman": "Женщина",
+"Country": "Страна, сельская местность",
+"Home": "Дом (жилище)",
+"Park": "Парк",
+"Place": "Место",
+"River": "Река",
+"School": "Школа",
+"Street": "Улица",
+"Town": "Город",
+"Bread": "Хлеб",
+"Breakfast": "Завтрак",
+"Dinner": "Обед",
+"Fish": "Рыба",
+"Food": "Еда",
+"Juice": "Сок",
+"Meat": "Мясо",
+"Milk": "Молоко",
+"Tea": "Чай",
+"Water": "Вода",
+"Afternoon": "День (время после полудня)",
+"Day": "День",
+"Evening": "Вечер",
+"Month": "Месяц",
+"Morning": "Утро",
+"Night": "Ночь",
+"Week": "Неделя",
+"Year": "Год",
+"Daughter": "Дочь",
+"Family": "Семья",
+"Father": "Отец",
+"Mother": "Мать",
+"Son": "Сын",
+"Box": "Коробка, ящик",
+"Bus": "Автобус",
+"Car": "Машина",
+"Clothes": "Одежда",
+"Cup": "Чашка",
+"Money": "Деньги",
+"Name": "Имя",
+"Pen": "Ручка",
+"Rain": "Дождь",
+"Thing": "Вещь, предмет",
+"Bad": "Плохой",
+"Beautiful": "Красивый",
+"Big": "Большой",
+"Cold": "Холодный",
+"Every": "Каждый",
+"Good": "Хороший",
+"Long": "Длинный",
+"New": "Новый",
+"Old": "Старый",
+"Short": "Короткий",
+"Small": "Маленький",
+"Warm": "Теплый",
+"Black": "Черный",
+"Blue": "Синий",
+"Brown": "Коричневый",
+"Green": "Зеленый",
+"Red": "Красный",
+"Yellow": "Желтый",
+"Never": "Никогда",
+"Now": "Сейчас",
+"Often": "Часто",
+"Today": "Сегодня",
+"Tomorrow": "Завтра",
+"Usually": "Обычно",
+"Yesterday": "Вчера"
+}
+
+RUEN = {
+"Я": "I",
+"Мы": "We",
+"Ты, вы": "You",
+"Он": "He",
+"Она": "She",
+"(о неодуш. предметах)": "It",
+"Они": "They",
+"Приходить": "Come",
+"Делать": "Do",
+"Пить": "Drink",
+"Есть": "Eat",
+"Давать": "Give",
+"Идти": "Go",
+"Иметь": "Have",
+"Слышать": "Hear",
+"Помогать": "Help",
+"Нравиться": "Like",
+"Жить": "Live",
+"Класть, положить": "Put",
+"Сказать": "Say",
+"Видеть": "See",
+"Сидеть": "Sit",
+"Спать": "Sleep",
+"Стоять": "Stand",
+"Брать": "Take",
+"Пользоваться, использовать": "Use",
+"Хотеть": "Want",
+"Работать": "Work",
+"Мальчик": "Boy",
+"Друг": "Friend",
+"Девочка": "Girl",
+"Мужчина, человек": "Man",
+"Люди": "People",
+"Женщина": "Woman",
+"Страна, сельская местность": "Country",
+"Дом (жилище)": "Home",
+"Парк": "Park",
+"Место": "Place",
+"Река": "River",
+"Школа": "School",
+"Улица": "Street",
+"Город": "Town",
+"Хлеб": "Bread",
+"Завтрак": "Breakfast",
+"Обед": "Dinner",
+"Рыба": "Fish",
+"Еда": "Food",
+"Сок": "Juice",
+"Мясо": "Meat",
+"Молоко": "Milk",
+"Чай": "Tea",
+"Вода": "Water",
+"День (время после полудня)": "Afternoon",
+"День": "Day",
+"Вечер": "Evening",
+"Месяц": "Month",
+"Утро": "Morning",
+"Ночь": "Night",
+"Неделя": "Week",
+"Год": "Year",
+"Дочь": "Daughter",
+"Семья": "Family",
+"Отец": "Father",
+"Мать": "Mother",
+"Сын": "Son",
+"Коробка, ящик": "Box",
+"Автобус": "Bus",
+"Машина": "Car",
+"Одежда": "Clothes",
+"Чашка": "Cup",
+"Деньги": "Money",
+"Имя": "Name",
+"Ручка": "Pen",
+"Дождь": "Rain",
+"Вещь, предмет": "Thing",
+"Плохой": "Bad",
+"Красивый": "Beautiful",
+"Большой": "Big",
+"Холодный": "Cold",
+"Каждый": "Every",
+"Хороший": "Good",
+"Длинный": "Long",
+"Новый": "New",
+"Старый": "Old",
+"Короткий": "Short",
+"Маленький": "Small",
+"Теплый": "Warm",
+"Черный": "Black",
+"Синий": "Blue",
+"Коричневый": "Brown",
+"Зеленый": "Green",
+"Красный": "Red",
+"Желтый": "Yellow",
+"Никогда": "Never",
+"Сейчас": "Now",
+"Часто": "Often",
+"Сегодня": "Today",
+"Завтра": "Tomorrow",
+"Обычно": "Usually",
+"Вчера": "Yesterday",
+}
+
+
+
+server = socket.socket(
+	socket.AF_INET,
+	socket.SOCK_STREAM,
+)
+
+server.bind(("127.0.0.1", 1235))
+server.listen(5)
+
+server_users = []
+listeners = []
+
+class thread_with_trace(threading.Thread):
+	def __init__(self, *args, **keywords):
+		threading.Thread.__init__(self, *args, **keywords) 
+		self.killed = False
   
-# Create a socket object 
-s = socket.socket()          
+	def start(self):
+		self.__run_backup = self.run 
+		self.run = self.__run       
+		threading.Thread.start(self) 
+	def __run(self):
+		sys.settrace(self.globaltrace) 
+		self.__run_backup() 
+		self.run = self.__run_backup 
   
-# Define the port on which you want to connect 
-port = 12345                
-  
-# connect to the server on local computer 
-s.connect(('127.0.0.1', port)) 
-  
-# receive data from the server 
-print s.recv(1024) 
-# close the connection 
-s.close()        
+	def globaltrace(self, frame, event, arg):
+		if event == 'call':
+			return self.localtrace
+		else:
+			return None
+	def localtrace(self, frame, event, arg):
+  		if self.killed:
+  			if event == 'line':
+  				raise SystemExit()
+  				return self.localtrace
+	def close(self):
+		self.killed = True
+
+
+LOG = ""
+
+	
+
+def listen_user(user_socket, id, address):
+	while True:
+		data = user_socket.recv(1024)
+		ask = data.decode()
+		global LOG
+		LOG += str(time.ctime(time.time())) + f"{address} ask about {ask} word\n"
+		
+		if ask in RUEN:
+			try:
+				user_socket.send(bytes(RUEN[ask], encoding="UTF-8"))
+			except BrokenPipeError:
+				pass
+		elif ask in ENRU:
+			try:
+				user_socket.send(bytes(ENRU[ask], encoding="UTF-8"))
+			except BrokenPipeError:
+				pass
+		elif ask == "end":
+			LOG += str(time.ctime(time.time())) + f"{address} disconnected\n"
+			break
+			user_socket.send(bytes('Thank You', encoding="UTF-8"))
+			listeners[id].close()
+		elif ask == "who":
+			try:
+				LOG += str(time.ctime(time.time())) + f"{address} who command\n"
+				user_socket.send(bytes('Eugene Goloven, 8 variant(Traslator)', encoding="UTF-8"))
+			except BrokenPipeError:
+				pass
+		else:
+			try:
+				LOG += str(time.ctime(time.time())) + f"{address} asked about {ask} word, but i don\'t know it\n"
+				user_socket.send(bytes('I don\'t know such word', encoding="UTF-8"))
+			except BrokenPipeError:
+				pass
+
+
+
+
+def start_server():
+	global LOG
+	LOG += str(time.ctime(time.time())) + "Server has been started\n"
+	print("Server has been started...")
+
+	try:
+		while True:
+			user_socket, address = server.accept()
+			print(f"User {address} has connected to the server")
+			
+			LOG += str(time.ctime(time.time())) + f"User {address} has connected to the server" + "\n"
+			server_users.append(user_socket)
+			user_socket.send(bytes(f"Hello user {len(server_users)}", encoding="UTF-8"))
+			listener = thread_with_trace(target=listen_user, args=(user_socket, len(listeners), address,))
+			listeners.append(listener)
+			listener.start()
+
+	except KeyboardInterrupt:
+		server.close()
+		LOG += str(time.ctime(time.time())) + "Server stoped\n"
+		for listener in listeners:
+			listener.close()
+		for user in server_users:
+			# user.disconnect()
+			user.send(bytes('end', encoding="UTF-8"))
+		with open("logs.txt", "w+") as file:
+			file.write(LOG)
+	
+
+
+if __name__ == '__main__':
+	start_server()
--- server.py
+++ server.py
@@ -1,36 +1,100 @@
-# first of all import the socket library 
-import socket                
+# # first of all import the socket library 
+# import socket                
   
-# next create a socket object 
-s = socket.socket()          
-print "Socket successfully created"
+# # next create a socket object 
+# s = socket.socket()          
+# print("Socket successfully created")
   
-# reserve a port on your computer in our 
-# case it is 12345 but it can be anything 
-port = 12345                
+# # reserve a port on your computer in our 
+# # case it is 12345 but it can be anything 
+# port = 12345                
   
-# Next bind to the port 
-# we have not typed any ip in the ip field 
-# instead we have inputted an empty string 
-# this makes the server listen to requests  
-# coming from other computers on the network 
-s.bind(('', port))         
-print "socket binded to %s" %(port) 
+# # Next bind to the port 
+# # we have not typed any ip in the ip field 
+# # instead we have inputted an empty string 
+# # this makes the server listen to requests  
+# # coming from other computers on the network 
+# s.bind(('', port))         
+# print("socket binded to %s" %(port))
   
-# put the socket into listening mode 
-s.listen(5)      
-print "socket is listening"            
+# # put the socket into listening mode 
+# s.listen(5)      
+# print("socket is listening")            
   
-# a forever loop until we interrupt it or  
-# an error occurs 
-while True: 
-  
-   # Establish connection with client. 
-   c, addr = s.accept()      
-   print 'Got connection from', addr 
-  
-   # send a thank you message to the client.  
-   c.send('Thank you for connecting') 
-  
-   # Close the connection with the client 
-   c.close() 
+# # a forever loop until we interrupt it or  
+# # an error occurs 
+# while True: 
+# # Establish connection with client. 
+# 	c, addr = s.accept()      
+# 	print('Got connection from', addr) 
+# 	# send a thank you message to the client.  
+# 	temp = 'Thank you for connecting'
+# 	c.send(temp.encode()) 
+
+# 	# c.send('Write a word to translate')
+
+# 	data = c.recv(1024)
+# 	if not data:
+# 		break
+# 	print(data)
+# 	c.send(data.upper())
+# 	# Close the connection with the client 
+
+
+import socket
+import threading
+import time
+
+
+address_to_server = ('localhost', 1235)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(address_to_server)
+
+
+
+LOG = ""
+USERID = ""
+
+LOG += str(time.ctime(time.time())) + f"I connected to the server\n"
+
+def listen_server():
+	global LOG
+	global USERID
+	while True:
+		try:
+			data = client.recv(1024)
+			answer = data.decode()
+			if answer == "end" :
+				client.close()
+				print("Server stoped responding")
+				LOG += str(time.ctime(time.time())) + f"Server stoped responding\n"
+				break
+			if "Hello user" in answer:
+				USERID = answer.split(" ")[2]
+			LOG += str(time.ctime(time.time())) + f"Server send me {answer}\n"
+			print(str(answer))
+		except OSError:
+			client.close()
+			print("disconected")
+			break
+
+listener = threading.Thread(target=listen_server)
+listener.start()
+
+while True:
+	try:
+		print(">", end="")
+		n = input()
+		LOG += str(time.ctime(time.time())) + f"I send {n} to server\n"
+		client.send(bytes(n, encoding="UTF-8"))
+		if n == "end":
+			break
+		time.sleep(0.1)
+	except OSError:
+		break
+
+with open(f"clientlogs{USERID}.txt", "w+") as file:
+			file.write(LOG)
+client.close()
+	
